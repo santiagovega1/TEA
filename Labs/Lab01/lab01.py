2f61cc5db3760df36e764884b6d9168a3c6c6434
Santiago Vega Silva (24009) <santiago.vega@est.zamorano.edu>
2022-09-12 23:29:40 -0600

    TEA Laboratorios

---
+++ Labs/Lab01/lab01.py
@@ -0,0 +1,6 @@
+# Tendencias e Innovacion en Tecnologias Agricolas (TEA)
+# Autor: Santiago Vega
+# Fecha: 2022.09.02
+# Editado: 2022.09.02
+
+print("Hola, mundo")
---
+++ Labs/Lab02/celsius2farenheit.py
@@ -0,0 +1,3 @@
+tempCelsius = float(input("Cual es la temperatura en Celsius?"))
+tempFarenheit = (9/5) * tempCelsius + 32
+print(tempFarenheit)
